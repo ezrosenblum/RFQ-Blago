a03d5dc02fa46b2f3492b3316f6b383bed435d46
Blagojche_Techup <blagojche@techup.me>
2025-07-15 18:37:31 +0200

    Local test

--- rfq-app/src/environments/environment.ts
+++ rfq-app/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://e6ace57eb99b.ngrok-free.app/api/v1/',
+  apiUrl: 'http://localhost:8090/api/v1/',
   appName: 'RFQ System',
   version: '1.0.0'
 };
