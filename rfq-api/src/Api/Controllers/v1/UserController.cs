17aba7b53d52ee256d78e4dd52408fb9bbdc752d
prendjov <prendjov.p@yahoo.com>
2025-07-31 10:43:21 +0200

    Added forgot/reset/change password endpoints on API.

--- rfq-api/src/Api/Controllers/v1/UserController.cs
+++ rfq-api/src/Api/Controllers/v1/UserController.cs
@@ -34,4 +34,77 @@ public class UserController : ApiControllerBase
         var response = await Mediator.Send(new UserGetCurrentDetailsQuery());
         return response;
     }
+
+
+    [HttpPut("change-password")]
+    public async Task<IActionResult> ChangePassword([FromBody] PasswordChangeCommand request)
+    {
+        await Mediator.Send(request);
+        return Ok();
+    }
+
+    [HttpPost("forgot-password")]
+    [AllowAnonymous]
+    public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordCommand request)
+    {
+        await Mediator.Send(request);
+        return Ok();
+    }
+
+    [HttpPost("reset-password")]
+    [AllowAnonymous]
+    public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordCommand request)
+    {
+        try
+        {
+            await Mediator.Send(request);
+            return Ok();
+        }
+        catch (ApplicationException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpPut("profile-picture")]
+    public async Task<MediaItemResponse> UpdateProfilePicture([FromForm] UserProfilePictureUpdateRequest request)
+    {
+        var response = await Mediator.Send(new UserProfilePictureUpdateCommand(request.Picture));
+        return response;
+    }
+
+    [HttpPut("activate")]
+    public async Task<IActionResult> Activate()
+    {
+        await Mediator.Send(new UserChangeStatusCommand(UserStatus.Active));
+        return Ok();
+    }
+
+    [HttpPut("deactivate")]
+    public async Task<IActionResult> Deactivate()
+    {
+        await Mediator.Send(new UserChangeStatusCommand(UserStatus.Deactivated));
+        return Ok();
+    }
+
+    [Authorize(Roles = "Administrator")]
+    [HttpGet("status")]
+    public async Task<IReadOnlyCollection<ListItemBaseResponse>> GetStatuses()
+    {
+        return await Mediator.Send(new GetEnumValuesQuery(typeof(UserStatus)));
+    }
+
+    [Authorize(Roles = "Administrator")]
+    [HttpGet("role/admin")]
+    public async Task<IReadOnlyCollection<ListItemBaseResponse>> GetRolesForAdmin()
+    {
+        return await Mediator.Send(new UserGetRolesQuery(true));
+    }
+
+    [AllowAnonymous]
+    [HttpGet("role")]
+    public async Task<IReadOnlyCollection<ListItemBaseResponse>> GetRoles()
+    {
+        return await Mediator.Send(new UserGetRolesQuery(false));
+    }
 }
