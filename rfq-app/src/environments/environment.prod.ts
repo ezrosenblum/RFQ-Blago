e7ece353a4dedb6c792d737e48f68c8bae2f6f70
Blagojche Josifoski <j_baze@live.com>
2025-08-01 02:07:58 +0200

    Update environment.prod.ts

--- rfq-app/src/environments/environment.prod.ts
+++ rfq-app/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8090/api/v1/',
+  apiUrl: 'https://api.rfq.techup.me/api/v1/',
   appName: 'RFQ System',
   version: '1.0.0'
 };
