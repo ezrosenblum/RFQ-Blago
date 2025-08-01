6096194de24af47e87c8d6ff44548608cd695761
ljupche-techup <ljubomir@techup.me>
2025-08-01 15:14:17 +0200

    Return angular files

--- rfq-app/src/environments/environment.ts
+++ rfq-app/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api/v1/',
+  apiUrl: 'http://localhost:8090/api/v1/',
   appName: 'RFQ System',
   version: '1.0.0'
 };
