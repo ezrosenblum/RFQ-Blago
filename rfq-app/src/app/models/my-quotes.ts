4f347421071fed31244e0aee6ac80c8f83fa96a7
ViktorijaUneva <viki.uneva123@gmail.com>
2025-08-15 19:51:34 +0200

    improved my quotes component

--- rfq-app/src/app/models/my-quotes.ts
+++ rfq-app/src/app/models/my-quotes.ts
@@ -1,4 +1,76 @@
 import { Paging, Sorting } from './notifications.model';
+import { LookupValue } from './user.model';
+
+export interface SubmissionUnit extends LookupValue {}
+export interface SubmissionStatus extends LookupValue {}
+
+export interface SubmissionUser {
+  id: number;
+  firstName: string;
+  lastName: string;
+  picture: string;
+}
+
+export interface Submission {
+  id: number;
+  title: string;
+  description: string;
+  quantity: number;
+  unit: SubmissionUnit;
+  status: SubmissionStatus;
+  jobLocation: string;
+  user: SubmissionUser;
+  submissionDate: Date; 
+  isValid: boolean;
+  streetAddress: string;
+  latitudeAddress: number;
+  longitudeAddress: number;
+}
+
+export interface CompanySize extends LookupValue {}
+
+export interface CompanyDetails {
+  id: number;
+  name: string;
+  contactPersonFirstName: string;
+  contactPersonLastName: string;
+  contactPersonEmail: string;
+  contactPersonPhone: string;
+  description: string;
+  streetAddress: string;
+  latitudeAddress: number;
+  longitudeAddress: number;
+  operatingRadius: number;
+  companySize: CompanySize;
+  certificateUrl: string | null;
+}
+
+export interface VendorStatus extends LookupValue {}
+
+export interface Vendor {
+  id: number;
+  firstName: string;
+  lastName: string;
+  picture: string;
+  phoneNumber: string;
+  suspensionReason: string | null;
+  email: string;
+  receiveEmailNotifications: boolean;
+  receivePushNotifications: boolean;
+  dateCreated: Date; 
+  status: VendorStatus;
+  companyDetails: CompanyDetails;
+}
+
+export interface QuoteMessageStatus extends LookupValue {}
+
+export interface QuoteMessageSender {
+  id: number;
+  firstName: string;
+  lastName: string;
+  picture: string;
+}
+
 
 export interface MyQuotesRequest {
   query?: string;
@@ -7,48 +79,55 @@ export interface MyQuotesRequest {
   submissionUserId?: number;
   priceFrom?: number | null;
   priceTo?: number | null;
-  validFrom?: string;
-  validTo?: string;
+  validFrom?: string; 
+  validTo?: string; 
   paging: Paging;
   sorting: Sorting;
 }
 
 export interface Quote {
+  submission: Submission;
   id: number;
-  vendorName: string;
-  vendorNumber: string;
   title: string;
   description: string;
   price: number;
-  currency: string;
-  rating: number;
-  successRate: number;
-  location: string;
-  validFrom: string;
-  validTo: string;
-  skills: string[];
-  profileImage?: string;
-  status: string;
-  statusId: number;
-  submissionDate?: Date;
-  warantyDuration?: number;
-  customerId?: number;
-  vendorId?: number;
-  quoteId?: number;
+  status: LookupValue;
+  quoteValidityIntervalType: LookupValue;
+  quoteValidityInterval: number;
+  validUntil: string; 
+  submissionId: number;
+  vendorId: number;
+  vendor: Vendor;
+  created: Date; 
+  timelineIntervalType: LookupValue;
+  minimumTimelineDuration: number;
+  maximumTimelineDuration: number;
+  warantyIntervalType: LookupValue;
+  warantyDuration: number;
+}
+
+export interface QuotesResponse {
+  items: Quote[];
+  pageNumber: number;
+  totalPages: number;
+  totalCount: number;
+  hasPreviousPage: boolean;
+  hasNextPage: boolean;
 }
 
 export interface MyQuotesList {
-  quotes: Quote[];
+  items: Quote[]; 
   totalCount: number;
   pageNumber: number;
-  pageSize: number;
   totalPages: number;
+  hasPreviousPage: boolean;
+  hasNextPage: boolean;
+  pageSize?: number; 
 }
-
 export interface FilterOptions {
   query: string;
   priceFrom: number | null;
   priceTo: number | null;
-  location: string;
-  minRating: number;
+  location?: string; 
+  minRating?: number; 
 }
