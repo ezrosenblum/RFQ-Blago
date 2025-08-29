a74c4f55c0a33f6cd4d12ec8730c8603c30d2d61
prendjov <prendjov.p@yahoo.com>
2025-08-29 08:35:52 +0200

    Removed using cache where it was an issue

--- rfq-api/src/Application/Features/Users/EventHandlers/UserUpdatedEventHandler.cs
+++ rfq-api/src/Application/Features/Users/EventHandlers/UserUpdatedEventHandler.cs
@@ -7,21 +7,15 @@ namespace Application.Features.Users.EventHandlers;
 
 public sealed class UserUpdatedEventHandler : INotificationHandler<UserUpdatedEvent>
 {
-    private readonly ICacheService _cacheService;
     private readonly IMediator _mediatr;
 
     public UserUpdatedEventHandler(
-        ICacheService cacheService,
         IMediator mediatr)
     {
-        _cacheService = cacheService;
         _mediatr = mediatr;
     }
     public async Task Handle(UserUpdatedEvent notification, CancellationToken cancellationToken)
     {
         await _mediatr.Send(new UserIndexCommand(notification.User.Id), cancellationToken);
-
-        await _cacheService.RemoveAsync($"{CacheKeys.User}-me-{notification.User.Id}", cancellationToken);
-        await _cacheService.RemoveAsync($"{CacheKeys.User}-{notification.User.Id}", cancellationToken);
     }
 }
